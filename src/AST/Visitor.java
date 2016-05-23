55a9cab1f3d9d4e13bf2a9c1816ddb295404eae1
João Bernardino <joao.mnb@gmail.com>
2016-05-24 00:07:16 +0100

    Working on Visitor

--- src/AST/Visitor.java
+++ src/AST/Visitor.java
@@ -2,6 +2,7 @@ package AST;
 
 import org.mozilla.javascript.Token;
 import org.mozilla.javascript.ast.AstNode;
+import org.mozilla.javascript.ast.InfixExpression;
 import org.mozilla.javascript.ast.NodeVisitor;
 import org.mozilla.javascript.ast.NumberLiteral;
 
@@ -12,9 +13,37 @@ public class Visitor implements NodeVisitor {
 
         System.out.println(node.makeIndent(node.depth()) + node.shortName() + " (" + node.getType() + " - " + Token.typeToName(node.getType()) + ")");
 
-        
+        switch (node.shortName()) {
+
+            case "InfixExpression":
+
+                return false;
+
+            default:
+
+        }
 
         return true;
+
+    }
+
+    private String print(AstNode node) {
+
+        switch (node.shortName()) {
+
+            case "InfixExpression":
+
+                return print((InfixExpression) node);
+
+            case "NumberLiteral":
+
+                return print((NumberLiteral) node);
+
+            default:
+
+        }
+
+        return "";
 
     }
 
@@ -24,9 +53,34 @@ public class Visitor implements NodeVisitor {
 
     }
 
-    private String printInfixExpression(AstNode node) {
+    private String print(InfixExpression node) {
 
-        return "";
+        String operator;
+
+        switch (node.getType()) {
+
+            case Token.ADD:
+                operator = "+";
+                break;
+
+            case Token.SUB:
+                operator = "-";
+                break;
+
+            case Token.MUL:
+                operator = "*";
+                break;
+
+            case Token.DIV:
+                operator = "/";
+                break;
+
+            default:
+                return "";
+
+        }
+
+        return print(node.getLeft()) + operator + print(node.getRight());
 
     }
 
